32e35fadc58ccc02465f3e53ce78cde118b8782f
prateek-skip <prateek.skip@gmail.com>
2024-12-26 18:34:59 +0530

    Game compliance api

--- src/api/routes/phoneRoutes.js
+++ src/api/routes/phoneRoutes.js
@@ -10,36 +10,36 @@ router.post("/prefill", async (req, res, next) => {
     const apiData = await phoneController.prefill(req.body)
     console.log(apiData)
     if(apiData["error"]){
-        res.status(apiData['error']['status']).json(challanData)    
+        res.status(apiData['error']['status']).json(apiData)    
         return;
     }
     res.status(200).json({
         message: "Request successfull.",
-        data : apiData
+        data : apiData.response
     })
 });
 
 router.post("/prefill-v2", async (req, res, next) => {
-    const apiData = await phoneController.panToName(req.body)
+    const apiData = await phoneController.prefillVersion2(req.body)
     if(apiData["error"]){
         res.status(apiData['error']['status']).json(apiData)    
         return;
     }
     res.status(200).json({
         message: "Request successfull.",
-        data : apiData
+        data : apiData.response
     })
 });
 
-router.post("/validate", async (req, res, next) => {
-    const apiData = await phoneController.validatePan(req.body)
+router.post("/gaming/compliance", async (req, res, next) => {
+    const apiData = await phoneController.gameCompliance(req.body)
     if(apiData["error"]){
         res.status(apiData['error']['status']).json(apiData)    
         return;
     }
     res.status(200).json({
         message: "Request successfull.",
-        data : apiData
+        data : apiData.response
     })
 });
 

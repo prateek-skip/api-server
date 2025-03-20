1c3b0545334c8259c077cd0231398d3bb00f0cd5
prateek-skip <prateek.skip@gmail.com>
2025-03-20 23:52:44 +0530

    Added logger for aadhaar

--- src/api/routes/aadharRoutes.js
+++ src/api/routes/aadharRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const aadharController = require("../controllers/Aadhar/aadharController");
-const { errorLogger } = require("../logger/logger");
+const { logger } = require("../logger/logger");
+const uuid = require("uuid")
 
 /**
  * GET request to /books
@@ -32,32 +33,63 @@ router.post("/verify-aadhar", async (req, res, next) => {
 });
 
 router.post("/send-otp", async (req, res, next) => {
-  const apiData = await aadharController.sendAadharOtp(req.body);
-  // if (!apiData["result"]) {
-  //   console.log(apiData)
-  //   res.status(422).json({ message: apiData.message });
-  //   return;
-  // }
-  res.status(200).json({
-    message: "Request successfull.",
-    data: apiData,
-  });
+  try {
+    req.correlationId = uuid.v4();
+    logger.info(`Incoming request: ${req.method} ${req.originalUrl}`, {
+      correlationId: req.correlationId,
+      requestBody: req.body,
+    });
+    const start = Date.now();
+
+    const apiData = await aadharController.sendAadharOtp(req.body);
+
+    const duration = Date.now() - start;
+    logger.info(
+      `Outgoing response: ${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`,
+      { correlationId: req.correlationId, requestBody: apiData }
+    );
+
+    res.status(200).json({
+      message: "Request successfull.",
+      data: apiData,
+    });
+  } catch (error) {
+    logger.error(
+      `Error response: ${req.method} ${req.originalUrl} ${res.statusCode}`,
+      { correlationId: req.correlationId, requestBody: error.message }
+    );
+    next(error);
+  }
 });
 
 router.post("/download-aadhar", async (req, res, next) => {
-  const apiData = await aadharController.downloadAadhar(req.body);
-  // if (!apiData["result"]) {
-  //   res.status(500).json({ message: apiData.message });
-  //   return;
-  // }
-  // if (apiData["status"] === 2) {
-  //   res.status(200).json({ message: apiData.message });
-  //   return;
-  // }
-  res.status(200).json({
-    message: "Request successfull.",
-    data: apiData,
-  });
+  try {
+    req.correlationId = uuid.v4();
+    logger.info(`Incoming request: ${req.method} ${req.originalUrl}`, {
+      correlationId: req.correlationId,
+      requestBody: req.body,
+    });
+    const start = Date.now();
+
+    const apiData = await aadharController.downloadAadhar(req.body);
+
+    const duration = Date.now() - start;
+    logger.info(
+      `Outgoing response: ${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`,
+      { correlationId: req.correlationId, requestBody: apiData }
+    );
+
+    res.status(200).json({
+      message: "Request successfull.",
+      data: apiData,
+    });
+  } catch (error) {
+    logger.error(
+      `Error response: ${req.method} ${req.originalUrl} ${res.statusCode}`,
+      { correlationId: req.correlationId, requestBody: error.message }
+    );
+    next(error);
+  }
 });
 
 module.exports = router;
